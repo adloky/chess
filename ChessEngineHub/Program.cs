a12cbdc0fd706b7a03e8ec3101d80fc7668aa060
Danil Antoshenkov <adloky@gmail.com>
2021-06-28 11:24:42 +0500

    add signalr hub

---
+++ ChessEngineHub/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
+using Chess;
+
+namespace ChessEngineHub {
+    class Program {
+        static void Main(string[] args) {
+            using (WebApp.Start("http://192.168.0.2:8080")) {
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+    public class EngineHub : Hub {
+        public string move(string fen, string move) {
+            string rFen = null;
+            try {
+                rFen = FEN.Move(fen, move);
+            } catch { }
+
+            return rFen;
+        }
+    }
+}
